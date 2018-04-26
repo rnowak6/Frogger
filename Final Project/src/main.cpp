553c6f18f28dbe5e574a05bbff0d9e9ce3f6c7ba
Rose <nowakarose@gmail.com>
2018-04-26 13:34:35 -0500

    moving images

--- Final Project/src/main.cpp
+++ Final Project/src/main.cpp
@@ -1,10 +1,12 @@
+#include "ofMain.h"
+#include "ofApp.h"
 #include <stdio.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_native_dialog.h>
 
-#define BACKGROUND_FILE "background.jpg"
-#define FROG_FILE    "frog.jpg"
+#define BACKGROUND_FILE "C:\\Users\\Rose\\source\\repos\\final-project-rnowak6\\Final Project\\resources\\background.jpg"
+#define FROG_FILE    "C:\\Users\\Rose\\source\\repos\\final-project-rnowak6\\Final Project\\resources\\frog.jpg"
 
 int main() {
 
@@ -90,7 +92,7 @@ int main() {
 		//creates the background and the sprite
 		if (draw) {
 			al_draw_bitmap(background, 0, 0, 0);
-			al_draw_bitmap(frog, x, y, NULL);
+			//al_draw_bitmap(frog, x, y, NULL);
 			al_flip_display();
 		}
 	}
